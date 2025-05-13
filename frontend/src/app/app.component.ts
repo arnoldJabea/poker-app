15be3542c0efc6d6518d398d7a1f027554f70ea4
Arnold Jabea <aroljabea71@gmail.com>
2025-05-13 10:42:08 +0200

    fin docker-front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment'; // adapte le chemin si nécessaire
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Poker-Front';
+  tables: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>(`${environment.backend_url}/tables`).subscribe({
+      next: (data) => {
+        this.tables = data;
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des tables', err);
+      }
+    });
+  }
 }
